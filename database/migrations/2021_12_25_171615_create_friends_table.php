6f959de2eae4bf6bdf46238ee70a5377b4d822bd
kobajun0219 <kobajunoasis@yahoo.co.jp>
2021-12-26 00:30:08 +0900

    add friend request

---
+++ database/migrations/2021_12_25_171615_create_friends_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table->id();
+            $table->BigInteger('request_id')->unsigned();
+            $table->BigInteger('receive_id')->unsigned();
+            $table->BigInteger('status');//0友達 1申請中 2申請拒否
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
